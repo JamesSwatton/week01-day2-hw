8ff4ca3affad966bbe8b77eae316097a9f23bd35
James Swatton <jamessswatton@gmail.com>
2019-04-09 17:34:33 +0100

    add Bear class and properties

--- specs/bear_spec.rb
+++ specs/bear_spec.rb
@@ -4,13 +4,17 @@ require_relative('../bear.rb')
 require_relative('../river.rb')
 require_relative('../fish.rb')
 
-class FishTest < MiniTest::Test
+class BearTest < MiniTest::Test
 
-# A fish should have a name
+  # A bear should have a name e.g. "Yogi" and a type e.g. "Grizzly"
+  #
+  # A bear should have an empty belly ( maybe an array... )
+  #
+  # A bear should be able to take a fish from the river
 
-def test_does_fish_have_name
-  fish = Fish.new("Nemo")
-  assert_equal("Nemo", fish.name)
+def test_does_bear_have_name
+  bear = Bear.new("Dave", "panda")
+  assert_equal("Dave", bear.name)
 end
 
 end
