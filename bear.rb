bb46acc1a9d474404b59ea12f3c75fd931a0276b
James Swatton <jamessswatton@gmail.com>
2019-04-09 18:21:47 +0100

    add take_fish_from_river method to Bear class

--- bear.rb
+++ bear.rb
@@ -8,8 +8,16 @@ class Bear
     @belly = []
   end
 
-
   def amount_of_fish_in_belly
     return @belly.count
   end
+
+  def take_fish_from_river(amount_of_fish, river)
+    count = 0
+    while count < amount_of_fish
+      @belly << river.fishes.pop
+      count += 1
+    end
+  end
+
 end
--- specs/bear_spec.rb
+++ specs/bear_spec.rb
@@ -6,8 +6,6 @@ require_relative('../fish.rb')
 
 class BearTest < MiniTest::Test
 
-  # A bear should be able to take a fish from the river
-
 # A bear should have a name e.g. "Yogi" and a type e.g. "Grizzly"
 def test_does_bear_have_name
   bear = Bear.new("Dave", "panda")
@@ -25,4 +23,12 @@ end
     assert_equal(0, bear.amount_of_fish_in_belly)
   end
 
+# A bear should be able to take a fish from the river
+  def test_if_bear_can_take_fish_from_river
+    river = River.new("Big", [1,2,3])
+    bear = Bear.new("Dave", "panda")
+    bear.take_fish_from_river(1, river)
+    assert_equal(1, bear.amount_of_fish_in_belly)
+    assert_equal(2, river.amount_of_fish)
+  end
 end
